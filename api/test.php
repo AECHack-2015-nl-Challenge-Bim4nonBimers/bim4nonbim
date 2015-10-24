0bffbddc086de48401ade6feb473cdbbab7493d3
Lawri van Buël <Lawri-van-Buel@users.noreply.github.com>
2015-10-24 17:18:47 +0200

    First working example of the API test

--- api/test.php
+++ api/test.php
@@ -3,6 +3,9 @@
  include("bimserverJsonConnector.class.php");
 
  $bimserver = new bimserverJsonConnector($bimserver["URL"], $bimserver["Name"], $bimserver["Key"]);
- var_dump($bimserver); 
- 
+ //var_dump($bimserver);
+
+ var_dump($bimserver->getAllProjects(false,false));
+
+ $bimserver->logout();
 ?>
